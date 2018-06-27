a11fce69fb7ec170b96e4a7d729835de7d6f2116
libang <libang@giga.build>
2018-06-27 14:03:14 +0800

    update socket io

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { SocketIo } from 'ng-io';
 
+@Injectable()
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private socket: SocketIo) {}
   title = 'app';
+
+  ngOnInit() {
+    console.log(1);
+    // this.socket.connect();
+    this.socket.on('connect', () => {
+      console.log('Connected');
+      this.socket.emit('events', { test: 'test' });
+    });
+    this.socket.on('events', data => {
+      console.log(data);
+    });
+    // this.socket.emit('events', 'test');
+    // this.socket.fromEvent('events').subscribe(data => console.log(data));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// import { WsService } from './Shared/ws.service';
 import { AppComponent } from './app.component';
+import { NgIoModule, NgIoConfig } from 'ng-io';
 
+const config: NgIoConfig = { url: 'ws://localhost:3000', options: {} };
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule, NgIoModule.forRoot(config)],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
